b8c2b030eb59e61fbb52c1251173fb32ffbed99d
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-01-21 11:55:58 +1300

    added new template.  finally figured out proper flow control

--- render.py
+++ render.py
@@ -1,6 +1,18 @@
 from secretary import Renderer
 engine = Renderer()
 
+def bool_to_list(boolean):
+    if boolean:
+        return [{}]
+    return []
+
+def inv_bool_to_list(boolean):
+    if not boolean:
+        return [{}]
+    return []
+
+engine.environment.filters['bool_to_list'] = bool_to_list
+engine.environment.filters['inv_bool_to_list'] = inv_bool_to_list
 
 def render_odt(form_name, values):
     with open('templates/' + form_name + '.odt') as template:
@@ -27,7 +39,11 @@ data = {
     }
 }
 
+
+data = {
+
+}
 if __name__ == "__main__":
-    result = render_odt('Authority & Instruction Form - Conveyancing', data)
+    result = render_odt('Land Transfer Tax Statement', data)
     with open('./.tmp/out.odt', 'wb') as f:
         f.write(result)
