887bced2434bbe6299a76171be272644582951e3
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-02-04 18:35:25 +1300

    second draft of cv10, including methodology for sums etc

--- render.py
+++ render.py
@@ -21,8 +21,28 @@ def week_day(date_string):
         return 'UNKNOWN'
 
 
+def sum_debits_credits(args, debits=True, credits=True):
+    total = 0
+    for arg in args:
+        if debits and arg and arg.get('debit'):
+            total -= arg.get('debit', 0)
+        if credits and arg and arg.get('credit'):
+            total += arg.get('credit', 0)
+    return total if debits and credits else abs(total)
+
+
+def format_number(number):
+    return '{:20,.2f}'.format(number or 0)
+
+def currency(string):
+    return '$%s' % string
+
+
 engine.environment.filters['join_and'] = join_and
 engine.environment.filters['week_day'] = week_day
+engine.environment.filters['sum_debits_credits'] = sum_debits_credits
+engine.environment.filters['format_number'] = format_number
+engine.environment.filters['currency'] = currency
 
 
 def render_odt(form_name, values):
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest
 from render import render_odt
-from collections import defaultdict
 import json
 
 
@@ -25,40 +24,54 @@ class TestRender(unittest.TestCase):
 
     def test_0001_letter_template(self):
         with open('fixtures/G01.json') as data:
-            render_odt('G01: Letter', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('G01: Letter',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0002_letter_of_engagement(self):
         with open('fixtures/G02.json') as data:
-            render_odt('G02: Letter of Engagement', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('G02: Letter of Engagement',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0003_file_closing_letter(self):
         with open('fixtures/G03.json') as data:
-            render_odt('G03: File Closing Letter', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('G03: File Closing Letter',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0004_letter_of_engagment_conveyancing(self):
         with open('fixtures/CV01.json') as data:
-            render_odt('CV01: Letter of Engagement - Conveyancing', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV01: Letter of Engagement - Conveyancing',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0005_settlment_undertakings_letter(self):
         with open('fixtures/CV03.json') as data:
-            render_odt('CV03: Settlement Undertakings Letter - Acting for Purchaser', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV03: Settlement Undertakings Letter - Acting for Purchaser',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0006_settlment_undertakings_letter_vendor(self):
         with open('fixtures/CV04.json') as data:
-            render_odt('CV04: Settlement Undertakings Letter - Acting for Vendor', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV04: Settlement Undertakings Letter - Acting for Vendor',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0007_mortgage(self):
         with open('fixtures/CV05.json') as data:
-            render_odt('CV05: Mortgage Discharge Request', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV05: Mortgage Discharge Request',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0008_settlement(self):
         with open('fixtures/CV06.json') as data:
-            render_odt('CV06: Vendors Settlement Letter', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV06: Vendors Settlement Letter',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
     def test_0009_settlement(self):
         with open('fixtures/CV07.json') as data:
-            render_odt('CV07: Letter to Financier Enclosing Originals', dict(json.loads(data.read()).items() + [("mappings", mappings)]))
+            render_odt('CV07: Letter to Financier Enclosing Originals',
+                       dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
+    def test_0010_trust_balance(self):
+        for d in xrange(1, 3):
+            with open('fixtures/CV10.%d.json' % d) as data:
+                render_odt('CV10: Trust Account Statement',
+                           dict(json.loads(data.read()).items() + [("mappings", mappings)]))
 
 if __name__ == '__main__':
     unittest.main()
